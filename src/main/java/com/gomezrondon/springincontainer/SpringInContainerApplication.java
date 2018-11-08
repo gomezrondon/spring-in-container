9b141ecaf932e7861db03dd4c2bee348f081b57d
Javier Gomez <Jgomez@cambridgeassociates.com>
2018-11-08 16:58:24 -0600

    1rst Commit.

---
+++ src/main/java/com/gomezrondon/springincontainer/SpringInContainerApplication.java
@@ -0,0 +1,12 @@
+package com.gomezrondon.springincontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInContainerApplication.class, args);
+	}
+}
