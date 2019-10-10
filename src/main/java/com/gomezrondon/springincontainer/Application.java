62ca3de4facdf8ceaa347e2d03e08b110a81374f
cajgomez <Jgomez@cambridgeassociates.com>
2019-10-10 13:19:38 -0600

    Upgrading JiB dependencies and changing docker-compose deployment script.

--- src/main/java/com/gomezrondon/springincontainer/Application.java
+++ src/main/java/com/gomezrondon/springincontainer/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringInContainerApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringInContainerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
